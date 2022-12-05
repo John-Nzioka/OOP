c25187158e5035c351536294abefc98c06c59ff7
John-Nzioka <nziokajohn254@gmail.com>
2022-12-05 11:46:27 +0300

    first commit

---
+++ includes/newclass.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+ class NewClass{
+     
+    //Properties and Methods goes here
+
+    public $info = "Am a developer from Ngongroad";
+ }
+
+ $object = new NewClass;
+ var_dump($object);
+
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+<body>
+
+  <?php
+    $pet01 = new Person();
+
+    echo $pet01->owner();
+
+  ?>  
+
+    
+</body>
+</html>
